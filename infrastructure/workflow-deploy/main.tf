8d03892b18b688d032806b9afc1c97cd5192b2fb
Justinas Pakalnis <justinas.p@inbox.lt>
2025-07-16 15:42:32 +0300

    wip

--- infrastructure/workflow-deploy/main.tf
+++ infrastructure/workflow-deploy/main.tf
@@ -1,7 +1,6 @@
 provider "google" {
-  credentials = file(var.credentials_path)
-  project     = var.project_id
-  region      = "europe-west1"
+  project = var.project_id
+  region  = "europe-west1"
 }
 
 resource "google_sql_user" "preview_user" {
